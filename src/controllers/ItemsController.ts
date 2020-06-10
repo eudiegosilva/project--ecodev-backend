48a9226c5b0f1f9260dd0f70b1105591e4cf982f
Diego Ramos <diego.coderamos@gmail.com>
2020-06-09 23:45:38 -0300

    api connection

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -10,7 +10,8 @@ class ItemsController {
       return {
         id: item.id,
         title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
+        // image_url: `http://localhost:3333/uploads/${item.image}`,
+        image_url: `http://172.28.43.173:3333/uploads/${item.image}`,
       };
     });
 
