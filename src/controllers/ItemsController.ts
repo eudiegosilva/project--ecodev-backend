3735308f49775e25eee8a8ffc17c7d8f1efe08fe
Diego Ramos <diego.coderamos@gmail.com>
2020-06-10 00:31:14 -0300

    update api URL

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -10,8 +10,8 @@ class ItemsController {
       return {
         id: item.id,
         title: item.title,
-        // image_url: `http://localhost:3333/uploads/${item.image}`,
-        image_url: `http://172.28.43.173:3333/uploads/${item.image}`,
+        // image_url: `http://192.168.0.104:3333/uploads/${item.image}`,//ubuntu
+        image_url: `http://172.28.43.173:3333/uploads/${item.image}`, //windows
       };
     });
 
